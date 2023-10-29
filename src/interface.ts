8f825da8704002b488fe1ef0234fbfffbfd41047
猫 <gfel@163.com>
2023-10-29 16:21:06 +0800

    fix: 修复Clients中存储对象丢失id的问题

--- src/interface.ts
+++ src/interface.ts
@@ -21,7 +21,7 @@ export type IMidwaySocketApplication = IMidwayApplication<
       NextFunction,
       undefined
     >;
-    clients: net.Socket[];
+    clients: IMidwaySocketContext[];
   }
 > &
   net.Server;
